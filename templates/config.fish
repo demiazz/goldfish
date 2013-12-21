47304fe4e7f97359bc4beea5448875250b9c3146
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-22 03:43:42 +0400

    Add template for user installation

---
+++ templates/config.fish
@@ -0,0 +1,19 @@
+#===== GOLDFISH PATH ===========================================================
+
+set fish_path $HOME/.goldfish
+
+#===== GOLDFISH CONFIG =========================================================
+
+# Uncomment and set list of needed plugins
+# set -f goldfish_plugins bundler rbenv
+
+# Uncomment and set theme
+# set -f goldfish_theme default
+
+#===== GOLDFISH LOADING ========================================================
+
+. $fish_path/goldfish.fish
+
+#===== FISH RC =================================================================
+
+# Your initialization code here.
