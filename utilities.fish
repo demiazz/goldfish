c9c22b57786aad6ec91577a0274f6c4bbd5a0805
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-22 13:37:58 +0400

    Add utility for binding alias, and enable completion for alias

--- utilities.fish
+++ utilities.fish
@@ -13,3 +13,13 @@ function gf_append_path
     set $path $argv[1] $$path
   end
 end
+
+# Enable completion for aliased commands.
+function gf_alias_completion --argument alias command
+    alias $alias $command
+
+    complete -c $alias -xa "(
+        set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
+        complete -C\"$command \$cmd\";
+    )"
+end
