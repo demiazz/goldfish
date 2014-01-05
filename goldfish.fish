ae426efdff9ec8f7af12d6ea5538c6c415f4441e
Igor Kapkov <igasgeek@me.com>
2014-01-05 11:02:34 +0400

    change namespaces fish dont have themes & plugins so couse we are framework we can use it without additional namespace. But fish_path can be misunderstood.

--- goldfish.fish
+++ goldfish.fish
@@ -4,11 +4,11 @@
 set fish_greeting ""
 
 # Load utilities
-. $fish_path/utilities.fish
+. $goldfish_path/utilities.fish
 
 # Load plugins
-for plugin in $goldfish_plugins
-  . $fish_path/plugins/$plugin.fish
+for plugin in $plugins
+  . $goldfish_path/plugins/$plugin.fish
 end
 
 # Load user plugins
@@ -18,8 +18,8 @@ end
 
 # Load theme
 if test -n theme
-  if test -d $fish_path/themes/$theme
-    set fish_function_path $fish_path/themes/$theme $fish_function_path
+  if test -d $goldfish_path/themes/$theme
+    set fish_function_path $goldfish_path/themes/$theme $fish_function_path
   else if test -d $HOME/.config/fish/themes/$theme
     set fish_function_path $HOME/.config/fish/themes/$theme $fish_function_path
   end
--- templates/config.fish
+++ templates/config.fish
@@ -1,18 +1,18 @@
 #===== GOLDFISH PATH ===========================================================
 
-set fish_path $HOME/.goldfish
+set goldfish_path $HOME/.goldfish
 
 #===== GOLDFISH CONFIG =========================================================
 
 # Uncomment and set list of needed plugins
-# set -g goldfish_plugins rbenv
+# set -g plugins rbenv
 
 # Uncomment and set theme
 # set -g theme default
 
 #===== GOLDFISH LOADING ========================================================
 
-. $fish_path/goldfish.fish
+. $goldfish_path/goldfish.fish
 
 #===== FISH RC =================================================================
 
