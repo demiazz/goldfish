8dba55fe96d04131ab38989a23eae4ad3f4f493d
Alexey Plutalov <demiazz.py@gmail.com>
2014-01-05 22:03:16 +0400

    Add fallback to $HOME/.config/fish/theme/*.fish

--- goldfish.fish
+++ goldfish.fish
@@ -6,10 +6,13 @@ set fish_greeting ""
 # Load goldfish functions
 set fish_function_path $fish_function_path $goldfish_path/functions
 
-# Auto update
+#===== AUTO UPDATING ===========================================================
+
 if not test $disable_update_checking = true
   check_for_update
 end
+
+#===== PLUGINS LOADING =========================================================
 
 # Load plugins
 for plugin in $plugins
@@ -21,11 +24,18 @@ for plugin in $HOME/.config/fish/plugins/*.fish
   . $plugin
 end
 
-# Load theme
-if test -n theme
+#===== THEME LOADING ===========================================================
+
+if set -q theme
   if test -d $goldfish_path/themes/$theme
     set fish_function_path $goldfish_path/themes/$theme $fish_function_path
   else if test -d $HOME/.config/fish/themes/$theme
     set fish_function_path $HOME/.config/fish/themes/$theme $fish_function_path
+  else if test -d $HOME/.config/fish/theme
+    set fish_function_path $HOME/.config/fish/theme $fish_function_path
+  end
+else
+  if test -d $HOME/.config/fish/theme
+    set fish_function_path $HOME/.config/fish/theme $fish_function_path
   end
 end
