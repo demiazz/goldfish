c3767785577caf1b9ad6cfc647c40b379d72851e
Alexey Plutalov <demiazz.py@gmail.com>
2014-01-26 18:38:02 +0400

    Reverse black and white for demiazz theme (right works with Solarized X colors now)

--- themes/demiazz/fish_prompt.fish
+++ themes/demiazz/fish_prompt.fish
@@ -12,39 +12,39 @@ function fish_prompt --description "Theme: demiazz"
       set_color -b black white
       printf ""
     case '*'
-      set_color -b blue white
+      set_color -b blue black 
       printf " %s " $USER
-      set_color -b white blue
+      set_color -b black blue
       printf ""
-      set_color -b black white
+      set_color -b white black
       printf ""
   end
 
   # current directory
 
-  set_color -b black white
+  set_color -b white black
   printf " %s" (prompt_pwd)
 
   # ruby version (via rbenv)
 
   if test $is_rbenv_enabled = 1
-    set_color -b black white
+    set_color -b white black 
     printf "  %s" (rbenv version-name)
   end
 
   # git branch
 
   if test -d ./.git
-    set_color -b black white
+    set_color -b white black
     printf "   %s" (git rev-parse --abbrev-ref HEAD)
   end
 
   # epilogue
 
-  set_color -b black white
+  set_color -b white black
   printf " "
-  set_color -b normal black
+  set_color -b normal white
   printf " "
 
-  set_color -b normal black
+  set_color -b normal white
 end
--- themes/demiazz/fish_right_prompt.fish
+++ themes/demiazz/fish_right_prompt.fish
@@ -5,11 +5,11 @@ function fish_right_prompt --description "Theme: demiazz"
     set __background red
   end
 
-  set_color -b normal white
+  set_color -b normal black
   printf " "
   set_color -b normal $__background
   printf ""
-  set_color -b $__background  white
+  set_color -b $__background black
   printf " %s " (date "+%H:%M")
   set_color -b normal $__background
 end
