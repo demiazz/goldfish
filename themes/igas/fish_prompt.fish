40738c54b1d5d887312f48badad875db5bf65b09
Igor Kapkov <igasgeek@me.com>
2014-03-09 22:10:10 +0400

    add time to igas theme

--- themes/igas/fish_prompt.fish
+++ themes/igas/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt --description "Theme: igas"
-  echo -n -s (set_color $fish_color_cwd) (pwd) (set_color normal) ' $ '
+  echo -n -s '[' (date +%H:%M) '] ' (set_color $fish_color_cwd) (pwd) (set_color normal) ' $ '
 end
